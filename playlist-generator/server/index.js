490db41b3b30d147725e50e2ebb34a5f769b2920
Will Cricchi <wcricchi@captechventures.com>
2021-04-13 11:11:12 -0400

    prod build created

--- playlist-generator/server/index.js
+++ playlist-generator/server/index.js
@@ -1,3 +1,4 @@
+const path = require ('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const pino = require('express-pino-logger')();
@@ -5,6 +6,7 @@ var cors = require('cors');
 var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 var request = require('request');
+
 
 
 const app = express();
@@ -16,13 +18,23 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(pino).use(cors())
   .use(cookieParser());
 
+app.use(express.static(path.join(__dirname, 'build')));
+
+
 var stateKey = 'spotify_auth_state';
 var client_id = 'f5c12e204437479da9efa53d9109b6ca'; // Your client id
 var redirect_uri = 'http://localhost:3000'; // Your redirect uri
 
 require('./routes')(app, stateKey, client_id, redirect_uri);
 
+if (process.env.NODE_ENV === 'production') {
+  app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
+}
 
 app.listen(3001, () =>
   console.log('Express server is running on localhost:3001')
 );
+
+module.exports = app;
